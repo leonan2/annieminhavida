fa27819a2299babbe6927c3fbb10186c9b1d9c0a
leonan2 <leonan2@hotmail.com>
2017-01-24 17:08:00 -0200

    Update fotos.php

--- fotos.php
+++ fotos.php
@@ -6,10 +6,10 @@
 <body>
   <div class="conteudo">
     <div id="fts">
-    <img src="img/imagem1.jpg" class="grow"  id="img1"> </img> 
-       <img src="img/imagem2.jpg" class="grow"  id="img1"> </img>
-     <img src="img/imagem3.jpg" class="grow"  id="img1"> </img>
-       <img src="img/imagem4.jpg" class="grow"  id="img1"> </img>
+    <img src="fotos/imagem1.jpg" class="grow"  id="img1"> </img> 
+       <img src="fotos/imagem2.jpg" class="grow"  id="img1"> </img>
+     <img src="fotos/imagem3.jpg" class="grow"  id="img1"> </img>
+       <img src="fotos/imagem4.jpg" class="grow"  id="img1"> </img>
     </div>
   <div>
 
