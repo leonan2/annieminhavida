f54f97ff3d07f178c014c20c6d43d15b88ce6b63
leonan2 <leonan2@hotmail.com>
2017-01-25 22:04:20 -0200

    Update fotos.php

--- fotos.php
+++ fotos.php
@@ -9,9 +9,9 @@
     <img src="fotos/annieandleo1.jpg" class="grow"  id="img1"> </img> 
        <img src="fotos/annieandleo2.jpg" class="grow"  id="img1"> </img>
      <img src="fotos/annieandleo6.jpg" class="grow"  id="img1"> </img>
-       <img src="fotos/annieandleo4.jpg" class="grow"  id="img1"> </img>
+       <img src="fotos/annieandleo3.jpg" class="grow"  id="img1"> </img>
  <img src="fotos/annieandleo5.jpg" class="grow"  id="img1"> </img>
- <img src="fotos/annieandleo3.jpg" class="grow"  id="img1"> </img>
+ <img src="fotos/annieandleo4.jpg" class="grow"  id="img1"> </img>
     </div>
   <div>
 
