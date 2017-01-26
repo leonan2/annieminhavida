fcccfa105913083ac1c6c7ad29a422a8e5ddab7d
leonan2 <leonan2@hotmail.com>
2017-01-25 22:08:39 -0200

    Update index.php

--- index.php
+++ index.php
@@ -56,8 +56,7 @@
     
                                                 <p class="decl">  <b>Eu te amo, Rosiane Freitas.</b>  <br>
                                                                          Volta pra mim? </p>
-    
-    <p><a href="fotos.php"> Nossas fotos! </a> </p>                                                    <p class="att"> Leonan Alves </p>
+      <p><a href="fotos.php"> Clique aqui! </a> </p>                     <p class="att"> Leonan Alves </p>
 
   </div>
 </div>
